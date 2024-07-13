ca7ddb053e068c509ee97fd283b14de329937d7c
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-07-13 21:02:03 +0530

    adding a instruction

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const OkBtn = document.getElementById('ok-btn');
 let timmer;
 
 submitBtn.addEventListener('click', ()=>{
+  document.getElementById('instruction').style.visibility = 'hidden';
   document.getElementById('text').classList.add('p-show');
   timmer = setInterval(popUp, 3000);
   // popUp();
@@ -11,12 +12,14 @@ submitBtn.addEventListener('click', ()=>{
 });
 
 function popUp(){
-
+  document.getElementById('text-2').classList.add('p-show');
   document.getElementById('container').classList.add('container-popUp');
 }
 
 OkBtn.addEventListener('click', ()=>{
+  document.getElementById('instruction').style.visibility = 'initial';
   document.getElementById('text').classList.remove('p-show');
+  document.getElementById('text-2').classList.remove('p-show');
   document.getElementById('container').classList.remove('container-popUp');
   clearInterval(timmer);
 });
