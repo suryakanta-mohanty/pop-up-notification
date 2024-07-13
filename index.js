c223821d9975f2bcea543c35f4c85a0824c0c486
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-07-13 20:27:10 +0530

    adding some information

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const OkBtn = document.getElementById('ok-btn');
 let timmer;
 
 submitBtn.addEventListener('click', ()=>{
-  timmer = setInterval(popUp, 1000);
+  document.getElementById('text').classList.add('p-show');
+  timmer = setInterval(popUp, 3000);
   // popUp();
   
 });
 
 function popUp(){
+
   document.getElementById('container').classList.add('container-popUp');
 }
 
 OkBtn.addEventListener('click', ()=>{
+  document.getElementById('text').classList.remove('p-show');
   document.getElementById('container').classList.remove('container-popUp');
   clearInterval(timmer);
 });
